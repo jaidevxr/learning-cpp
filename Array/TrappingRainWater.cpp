b5a8693ca32a76208feab8d3bfbe1edf6d14ec10
jaidevxr <jaiy9956@gmail.com>
2026-01-18 00:17:15 +0530

    Add program to calculate the amount of trapped rainwater in an array

---
+++ Array/TrappingRainWater.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxWater(vector<int>& arr) {
+    int res = 0;
+
+    for (int i = 1; i < arr.size() - 1; i++) {
+
+        int left = arr[i];
+        for (int j = 0; j < i; j++)
+            left = max(left, arr[j]);
+
+  
+        int right = arr[i];
+        for (int j = i + 1; j < arr.size(); j++)
+            right = max(right, arr[j]);
+
+        res += (min(left, right) - arr[i]);
+    }
+
+    return res;
+}
+
+int main() {
+    vector<int> arr = { 2, 1, 5, 3, 1, 0, 4 };
+    cout << maxWater(arr);
+    return 0;
+}
